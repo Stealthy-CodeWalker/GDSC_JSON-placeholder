187745f7a4c6557b3ab00c8aebe0e4abe1a31996
Stealthy-CodeWalker <webdev.vanguard@gmail.com>
2023-03-23 18:29:41 +0530

    features implemented

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GDSC_JSON-placeholder
 Technical task for GDSC
+
+Random text generator:
+Create an application which fetches and displays a text from https://jsonplaceholder.typicode.com/posts. 
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [post, setPost] = useState([])
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/posts`)//fetching JSON data as a promise object
+    .then((response)=>response.json())//parsing the promise object as a json file
+    .then((data)=>setPost(data))//setting the json data to the hook
+    .catch((err)=>console.log(err))
+},[]);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="body">
+      {post.map((e)=>//mapping the hook data
+      (
+          <div className='container'>
+            <p><b>userId</b>: {e.userId}</p>
+            <p><b>id</b>: {e.id}</p>
+            <p><b>title</b>: {e.title}</p>
+            <p><b>body</b>: {e.body}</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
